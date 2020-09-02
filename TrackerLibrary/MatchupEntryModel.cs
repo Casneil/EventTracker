d1961c2c927f17c543c27de81efa751c01f1a05b
Casneil Simpson <casneil.simpson@gmail.com>
2020-09-02 21:18:12 +0200

    Adding comments

--- TrackerLibrary/MatchupEntryModel.cs
+++ TrackerLibrary/MatchupEntryModel.cs
@@ -6,8 +6,17 @@ namespace TrackerLibrary
 {
     public class MatchupEntryModel
     {
+        /// <summary>
+        /// Represents one team in the entire matchup.
+        /// </summary>
         public TeamModel TeamCompeting { get; set; }
+        /// <summary>
+        /// Represents the score for this team.
+        /// </summary>
         public double Score { get; set; }
+        /// <summary>
+        /// Represents the matchup this team came from
+        /// </summary>
         public MatchUpModel ParentMatchup { get; set; }
     }
 }
